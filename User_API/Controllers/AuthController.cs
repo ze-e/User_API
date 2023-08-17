799564a3f02225e066a96024d92aafaa426970d9
ZEE <white.noise616@gmail.com>
2023-08-17 15:35:00 -0600

    init

---
+++ User_API/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using BCrypt.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using User_API.Models;
+
+namespace User_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		public static User user = new User();
+
+		[HttpPost("register")]
+		public ActionResult<User> Register(UserDto request)
+		{
+			string passwordHash =
+				BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+			user.Username = request.Username;
+			user.PasswordHash = passwordHash;
+
+			return Ok(user);
+		}
+		[HttpPost("login")]
+		public ActionResult<User> Login(UserDto request)
+		{
+			
+			if (user.Username != request.Username)
+			{
+				return BadRequest("User not found");
+			}
+
+			if(!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash)) {
+				return BadRequest("Wrong password");
+			}
+
+			return Ok(user);
+		}
+	}
+}
---
+++ User_API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace User_API.Models
+{
+	public class User
+	{
+		public string Username { get; set; } = string.Empty;
+		public string PasswordHash { get; set; } = string.Empty;
+	}
+}
